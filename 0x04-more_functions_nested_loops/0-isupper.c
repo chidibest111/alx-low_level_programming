3a9ce90f2a68f0799d02c74c6fe369b1e2c35e22
chidibest111 <rapheal4chidibest2@gmail.comgit config -- global user.email rapheal4chidibest2@gmail.com>
2023-10-13 23:09:58 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper - detect upper case alphabet
+ *
+ * @c: input character
+ *
+ * Return character
+ *
+ * Return: (1) for true (0) false
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'A && c= 'Z')
+	{
+		return (1);
+	}
+	else
+	}
+		return (0);
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Project in c language
